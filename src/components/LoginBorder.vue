d2fa68b26494a4dc5c3f3152b0d8c7c338fa77cd
sepide <sepidetajik@yahoo.com>
2019-11-01 22:04:15 -0700

    ui improvements

--- src/components/LoginBorder.vue
+++ src/components/LoginBorder.vue
@@ -4,13 +4,12 @@
             ورود به حساب شخصی
         </p>
         <p>
-            <input required v-model="username" @blur="checkUsername()" v-bind:style ="checkUsername() ? 'border: 2px solid green;' : 'border: 2px solid red;' "
- type="text" name="username" id="username"  class="miniborderitems" placeholder="نام کاربری یا پست الکترونیک">
+            <input required v-model="username" @blur="checkUsername()"  type="text" name="username" id="username"  class="miniborderitems" placeholder="نام کاربری یا پست الکترونیک">
         </p>
         <p>
-            <input required v-model="password" @blur="checkPassword" v-bind:style ="checkPassword() ? 'border: 2px solid green;' : 'border: 2px solid red;'" type="password" name="password" id= "password" class="miniborderitems2" placeholder="کلمه عبور">
+            <input required v-model="password" @blur="checkPassword()"  type="password" name="password" id= "password" class="miniborderitems2" placeholder="کلمه عبور">
         </p>
-        <button class="miniborderbutton">
+        <button class="miniborderbutton button5">
             ورود
         </button>
         
@@ -32,10 +31,10 @@
 
         checkUsername: function(){  //TODO: body should be implemented
            if(this.username.length > 3){
-               console.log("username is fine");
+              // console.log("username is fine");
                return true;
            }else{
-               console.log("username is wrong");
+            //   console.log("username is wrong");
                //TODO: change style of the username field
                return false;
            }
@@ -43,11 +42,11 @@
 
         checkPassword: function(){  //TODO: body should be implemented
             if(this.password.size> 10){
-                console.log("password is fine");
+              //  console.log("password is fine");
                 //TODO: change style of the username field
                 return true;
             }else{
-                console.log("password is wrong");
+               // console.log("password is wrong");
 
                 //TODO: change style of the username field
                 return false;
@@ -80,22 +79,50 @@
     height : 35px;
     margin-bottom: 3%;
     text-align: center;
+    border:2px solid palevioletred ;
 }
 .miniborderitems2{
     width : 70%;
     height : 35px;
     margin-bottom: 30%;
     text-align: center;
+    border: 2px solid palevioletred;
 }
 .miniborderbutton{
-    width: 40%;
-    height: 30px;
-    margin-bottom: 20%;
+
+  background-color: white; /* Green */
+  border: 2 px solid purple;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+  margin-bottom: 20%;
+  border-radius: 20%;
+  width: 30%;
+}
+.button5 {
+  background-color: white;
+  color: black;
+  border: 2px solid #555555;
+}
+
+
+.miniborderbutton:hover {
+  background-color: #9276a8;
+  color: white;
 }
 .minibordertext{
     margin-top: 5%;
+    margin-bottom:10% ;
     font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     font-size: 20;
+    font-weight: bold;
     color:darkorchid;
 }
 </style>
