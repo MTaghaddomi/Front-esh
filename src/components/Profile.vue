2954455a54d58d96ac35f4a8b8a96030bb2a5414
m-parchami <m.parchami426@gmail.com>
2019-11-16 12:10:54 +0330

    stored Token in browser, added getProfile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -13,18 +13,21 @@ export default {
     
     computed:{
         userData: function(){
-            return store.state
+            return store.state.userProfile
         }
     },
     beforeRouteEnter : (to,from,next)=>{
 
-              console.log("checking authentication for entering /profile ")
-              if( ! store.state.username ){
-                  console.log("nashod ke beshe")
-                  next('/')
-              }
-              console.log("shod ke beshe")
-              next()
+            console.log("checking authentication for entering /profile ")
+            store.dispatch('getProfile')
+            console.log(localStorage.getItem('token'))
+            if(loggedin){
+                console.log("logged in successfuly, and got your profile")
+                next()
+            }else{
+                console.log("failed to login to get your profile")
+                next('/')
+            }
             
     }
 }
--- src/store.js
+++ src/store.js
@@ -6,16 +6,35 @@ Vue.use(Vuex)
 import customAxios from './customAxios'
 export default new Vuex.Store({
     state: {
-        token :"",
-        username: "",
-        userProfile: null
+        serverUsername: "",
+        userProfile: {username: "", firstName: "", lastName: "",email:"",birthday:"",phoneNumber:""},
+        loggedin: false
     },
     mutations: {
-        saveLogin(state,registerData){
-            console.log('saving the state')
-             state.token = registerData.token;
-             state.username = registerData.username      ;
-        }  
+        saveLogin(state,serverData){
+            console.log('saving the state and cookie')
+            state.serverUsername = serverData.serverUsername;         
+            localStorage.setItem('token',serverData.token)
+            state.loggedin= true
+        },
+        deleteLogin(state){
+            console.log("deleting the state and cookie")
+            state.serverUsername = ""
+            userProfile = null;
+            localStorage.removeItem('token')
+            state.loggedin = false
+        },
+        saveProfile(state,serverData){
+            console.log("saving profile on the state")
+            state.userProfile.username= serverData.username
+            state.userProfile.firstName= serverData.firstName
+            state.userProfile.lastName=serverData.erverDatalastName
+            state.userProfile.email= serverData.email
+            state.userProfile.birthday= serverData.birthday
+            state.userProfile.phoneNumber=serverData.phoneNumber
+            state.loggedin = true
+        }
+        
     },
     actions: {
         signup({commit,state},newRegister){
@@ -27,30 +46,26 @@ export default new Vuex.Store({
                 console.log(res);
                 commit('saveLogin', {
                      token: res.data.token,
-                     username: res.data.username
+                     serverUsername: res.data.username
                 })
-                
                 //TODO: ROUTE TO THE USER'S PROFILE SECTION
                 console.log("routing to the /profile")
                 this.$router.push('/profile')
-
             })
             .catch((error)=>{
                 console.log("register failed"); 
-                console.log(error);
+                console.log(error);  
+                commit('deleteLogin')
             })
-
 
             console.log("the following has been saved: ")
             console.log(state)
+            console.log(localStorage.getItem('token'))
 
         },
+
         login({commit,state}, newLogin){
             console.log("logging in: ",newLogin)
-
-            // commit('saveLogin',{token: newLogin.password,username: newLogin.username})                 //just for testing locally
-            // console.log(state)
-            // console.log('000000000')
             customAxios.post('/users/login',newLogin)
             .then((res)=>{
                 console.log("success on login");
@@ -63,17 +78,47 @@ export default new Vuex.Store({
                 //TODO: ROUTE TO THE USER'S PROFILE SECTION
                 console.log("routing to the /profile")
                 this.$router.push('/profile')
+                // this.dispatch.getProfile()
 
             })
             .catch((error)=>{
                 console.log("login failed"); 
                 console.log(error);
+                commit('deleteLogin')
             })
 
             console.log("the following has been saved: ")
             console.log(state)
+            console.log(localStorage.getItem('token'))
 
         },
+
+        logout({commit}){
+            console.log("logging out")
+            commit('deteleLogin')
+            this.$router.push('/login')
+        },
+
+        getProfile({commit,state}){
+            console.log("getting user profile")
+            customAxios.post('/users/'+state.serverUsername,{
+                headers: {'Auth':localStorage.getItem('token')}
+            })
+            .then((res)=>{
+                console.log("success on getting profile")
+                commit('saveProfile',res.data)
+            }).catch((err)=>{
+                console.log("failed to get the user profile")
+                // commit('deleteLogin')
+            })
+            
+        }
+        // ,
+        // checkToken(){
+        //     console.log("checking authentication status")
+
+            
+        // }
 
     },
 });
