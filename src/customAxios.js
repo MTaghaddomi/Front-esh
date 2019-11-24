fa07f2da7ff0cf418da6168c09e9019afc50f995
m-parchami <m.parchami426@gmail.com>
2019-11-24 12:14:07 +0330

    added cross origin header

--- src/customAxios.js
+++ src/customAxios.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 const customAxios = axios.create({
 
      //configure the customAxios instance
+     headers:{
+          'Acess-Control-Allow-Origin':'https://system-analysis-design.herokuapp.com'
+     }
 
 });
 customAxios.defaults.baseURL = 'https://system-analysis-design.herokuapp.com' //setting baseURL for later requests
