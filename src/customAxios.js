af45231fdefc01f9aab241680108dffd0d2b034e
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2019-11-25 17:53:16 +0330

    pull req

--- src/customAxios.js
+++ src/customAxios.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 const customAxios = axios.create({
 
      // configure the customAxios instance
-     // headers:{
-     //      'Access-Control-Allow-Origin':'*',
-     // }
+     headers:{
+          'Access-Control-Allow-Origin':'http://localhost:8080',
+     }
 
 
 });
-customAxios.defaults.baseURL = 'http://localhost:5000' //setting baseURL for later requests
+customAxios.defaults.baseURL = 'http://localhost:8000' //setting baseURL for later requests
 customAxios.defaults.withCredentials = false
 export default customAxios
